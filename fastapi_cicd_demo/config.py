58fa81fc20168d6829865c933acb56974f8f1655
Sam Yao <turisesonia@gmail.com>
2020-12-01 15:19:39 +0800

    feat: add config and db connection infomation

---
+++ fastapi_cicd_demo/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+# DB_HOST=
+# DB_USERNAME=
+# DB_PASSWORD=
+
+DB_HOST = os.getenv("DB_HOST", "")
+DB_DATABASE = os.getenv("DB_DATABASE", "")
+DB_USERNAME = os.getenv("DB_USERNAME", "")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
